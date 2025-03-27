a47d7d20ab66980339e70384c8c89b0378276ded
Ares8989 <candy.pat@gmail.com>
2025-03-27 12:18:37 -0700

    Made the voting system work, added control review (skeleton)

--- java_script/review_rating.js
+++ java_script/review_rating.js
@@ -1,27 +1,51 @@
-ratings=[];//make an array to hold the ratings of the reviews
+function initializeRatingSystem(){
+    //check if the ratings array already exists
+    let ratings=readFromRatingSystem();
+    if (ratings==undefined){//initialize the array if it didn't exist
+        ratings=[];
+        localStorage.setItem("ratingsJSON",JSON.stringify(ratings));       
+    }
+    return ratings;
+}
 
-function findIndex(reviewName){
+function readFromRatingSystem(){
+    let rawRatingsString=localStorage.getItem("ratingsJSON");
+    let ratings = JSON.parse(rawRatingsString);
+    return ratings;
+}
+
+function writeToRatingSystem(ratings){
+    localStorage.setItem("ratingsJSON",JSON.stringify(ratings));
+    return;
+}
+
+
+function findIndex(reviewName, ratings){
     let i=0;
     for (; i < ratings.length;i++){
         if (ratings[i].name==reviewName){
             return i;//return the index of the array where the review was found
         }
     }
-    createReviewObj(reviewName);
+    createReviewObj(reviewName, ratings);
     return i++;
 }
 
-function upvoteReview(reviewName){
-
+function upvoteReview(index, ratings){
+    ratings[index].popularity++;
+    return;
 }
 
-function downvoteReview(reviewName){
-    
+function downvoteReview(index, ratings){
+    ratings[index].popularity--;
+    return;    
 }
 
-function createReviewObj(reviewName){
+function createReviewObj(reviewName, ratings){
     reviewObj={
         name: reviewName,
         popularity: 0
     }
+    ratings.push(reviewObj);
+    return;
 }
