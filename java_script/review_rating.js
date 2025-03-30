0d77c8e4bb1dde9cbca626c5ace8e1e08607d5de
Ares8989 <candy.pat@gmail.com>
2025-03-30 11:45:59 -0700

    made the top game list also be links

--- java_script/review_rating.js
+++ java_script/review_rating.js
@@ -21,6 +21,17 @@ function writeToRatingSystem(ratings){
 }
 
 
+// function findIndex(reviewName, ratings){
+//     let i=0;
+//     for (; i < ratings.length;i++){
+//         if (ratings[i].name==reviewName){
+//             return i;//return the index of the array where the review was found
+//         }
+//     }
+//     createReviewObj(reviewName, ratings);
+//     return i++;
+// }
+
 function findIndex(reviewName, ratings){
     let i=0;
     for (; i < ratings.length;i++){
@@ -28,8 +39,7 @@ function findIndex(reviewName, ratings){
             return i;//return the index of the array where the review was found
         }
     }
-    createReviewObj(reviewName, ratings);
-    return i++;
+    return -1;
 }
 
 function upvoteReview(reviewName, ratings){
@@ -44,30 +54,69 @@ function downvoteReview(reviewName, ratings){
     return;    
 }
 
-function createReviewObj(reviewName, ratings){
-    reviewObj={
-        name: reviewName,
-        popularity: 0
+// function createReviewObj(reviewName, ratings){
+//     reviewObj={
+//         name: reviewName,
+//         popularity: 0
+//     }
+//     ratings.push(reviewObj);
+//     return;
+// }
+
+function createReviewObj(reviewName,documentName,ratings){
+    let index=findIndex(reviewName, ratings);
+    //this game didnt have a review already
+    if (index==-1){
+        reviewObj={
+            name: reviewName,
+            location:documentName,
+            popularity: 0
+        }
+        ratings.push(reviewObj);
+        return;
     }
-    ratings.push(reviewObj);
     return;
 }
 
 function printTopFive(ratings){
     if(ratings[0] != undefined){
         document.getElementById("Review0").innerText=ratings[0].name;
+        document.getElementById("Review0").href=ratings[0].location;
     }
     if(ratings[1] != undefined){
         document.getElementById("Review1").innerText=ratings[1].name;
+        document.getElementById("Review1").href=ratings[1].location;
     }
     if(ratings[2] != undefined){
         document.getElementById("Review2").innerText=ratings[2].name;
+        document.getElementById("Review2").href=ratings[2].location;
     }
     if(ratings[3] != undefined){
         document.getElementById("Review3").innerText=ratings[3].name;
+        document.getElementById("Review3").href=ratings[3].location;
     }
     if(ratings[4] != undefined){
         document.getElementById("Review4").innerText=ratings[4].name;
+        document.getElementById("Review4").href=ratings[4].location;
     }
     return;    
 }
+
+// function printTopFive(ratings){
+//     if(ratings[0] != undefined){
+//         document.getElementById("Review0").innerText=ratings[0].name;
+//     }
+//     if(ratings[1] != undefined){
+//         document.getElementById("Review1").innerText=ratings[1].name;
+//     }
+//     if(ratings[2] != undefined){
+//         document.getElementById("Review2").innerText=ratings[2].name;
+//     }
+//     if(ratings[3] != undefined){
+//         document.getElementById("Review3").innerText=ratings[3].name;
+//     }
+//     if(ratings[4] != undefined){
+//         document.getElementById("Review4").innerText=ratings[4].name;
+//     }
+//     return;    
+// }
